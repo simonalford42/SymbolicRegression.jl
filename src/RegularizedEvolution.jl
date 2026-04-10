906445574f746e11d1013f851aab659131f1e098
Simon Alford <sca63@cornell.edu>
2026-04-10 01:27:32 -0400

    exp34: 20% random 2nd parent (vs 100% in exp24), revert ConstantOptimization to exp14

--- src/RegularizedEvolution.jl
+++ src/RegularizedEvolution.jl
@@ -80,8 +80,12 @@ function reg_evol_cycle(
 
         else # Crossover
             allstar1 = apply_custom_selection(pop, running_search_statistics, options)
-            # exp24: random second parent for more structural diversity
-            allstar2 = pop.members[rand(1:pop.n)]
+            # exp34: 20% random 2nd parent, 80% tournament (less disruptive than exp24's 100%)
+            allstar2 = if rand() < 0.20
+                pop.members[rand(1:pop.n)]
+            else
+                apply_custom_selection(pop, running_search_statistics, options)
+            end
 
             crossover_recorder = RecordType()
             baby1, baby2, crossover_accepted, tmp_num_evals = crossover_generation(
